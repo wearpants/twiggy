1f56c2305d3dbac9f090b51b8b88b1b57fc8beaa
Peter Fein <pete@wearpants.org>
2010-03-25 22:50:35 -0500

    even better futzing

--- futz.py
+++ futz.py
@@ -1,6 +1,7 @@
 import twiggy
-
 twiggy.basicConfig()
+
+## User-facing features
 
 # works out of the box; standard levels
 twiggy.log.debug('You may not care')
@@ -9,15 +10,27 @@ twiggy.log.error('OMFG! Pants on fire!')
 # support for format strings
 twiggy.log.info('I like %s', "bikes")
 
-# new style too
+# new style are preferred tho
 twiggy.log.info('I wear {0} on my {where}', 'pants', where='legs')
 
 # named loggers
 mylog = twiggy.log.name('alfredo')
 mylog.debug('hello')
 
+## Better output
+# Newlines are suppressed by default; that can be turned off per-message
+twiggy.log.info('user\ninput\nannoys\nus')
+twiggy.log.options(suppress_newlines=False).info('we\ndeal')
+
+# Exceptions.  Can also pass exc_info. Each line is prefixed.
+try:
+    1/0
+except:
+    twiggy.log.trace('error').warning('oh noes') # error is the default & can be omitted
+
+## Method Chaining
 # this is not getLogger tho
-twiggy.log.name('alfredo') is not mylog
+print twiggy.log.name('alfredo') is not mylog
 
 # You don't need to store loggers
 # I like this chained style a lot.
@@ -37,16 +50,7 @@ per_request_log.info('sending page over tubes')
 # Chained style is awesome
 twiggy.log.name('donjuan').fields(pants='sexy').info('hello, ladies')
 
-# Exceptions.  Can also pass exc_info. Each line is prefixed.
-try:
-    1/0
-except:
-    twiggy.log.trace('error').warning('oh noes') # error is the default & can be omitted
-
-# Newlines are suppressed by default; that can be turned off per-message
-twiggy.log.info('user\ninput\nannoys\nus')
-twiggy.log.options(suppress_newlines=False).info('we\ndeal')
-
+## Dynamic
 # More fun. Any functions in args/fields are called and the value substitued.
 import os
 import threading
@@ -56,3 +60,25 @@ def thread_name():
     return threading.currentThread().getName()
 
 twiggy.log.fields(pid=os.getpid).info("I'm in thread {0}", thread_name)
+
+## Optimizations
+# loggers can take a min_level
+mylog.min_level = twiggy.Levels.INFO
+mylog.info("You see this")
+mylog.debug("This is hidden")
+
+# Also take a filter that operates on format_spec. Use case is efficiently
+# shutting off specific messages in a library which is doing something stupid
+mylog.filter = lambda s: "shenanigans" not in s
+mylog.info("Starting silliness")
+for i in xrange(3): # or much larger
+    mylog.info("I call shenanigans!")
+mylog.info("End silliness")
+
+
+## Questions:
+
+## Only required attributes of message now are level & format_spec (the text).
+## I've thought about making name mandatory (eh). Going the other way, I've
+## thought about making level optional (too flexible, and easy enough to
+## ignore - see struct())
