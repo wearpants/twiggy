bf1fbb49f170d3b285eaeb9ce9cc26ab227eb5d9
Peter Fein <pete@wearpants.org>
2010-10-15 12:46:23 -0700

    add googleanalytics

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ sys.path.append(os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinxcontrib.googleanalytics']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -85,6 +85,10 @@ pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
+
+# -- Various custom options ----------------------------------------------------
+
+googleanalytics_id = "UA-3052036-8"
 
 # docttest support
 
