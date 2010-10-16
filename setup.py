792a6d3d7fa3d85ddbd1c46d72426fcf39666a9f
Peter Fein <pete@wearpants.org>
2010-10-15 18:51:27 -0700

    add README

--- setup.py
+++ setup.py
@@ -16,11 +16,5 @@ setup(name='Twiggy',
       "Development Status :: 3 - Alpha",
       "Intended Audience :: Developers",
       "License :: OSI Approved :: BSD License",],
-      long_description="""Twiggy is a Pythonic logger
-      
->>> log.name('frank').fields(number=42).info("hello {who}, it's a {} day", 'sunny', who='world')
-INFO:frank:number=42:hello world, it's a sunny day
-
-You should use Twiggy because it is awesome. For more information, `see this blog post <http://blog.wearpants.org/meet-twiggy>`_ or the `project homepage <http://twiggy.wearpants.org>`_.
-"""
-)
+      long_description=open('README').read(),
+      )
