510507693962570eb86b31b915c9e7e98d58e199
Peter Fein <pete@wearpants.org>
2010-10-15 19:30:26 -0700

    rebump version numbers

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='Twiggy',
-      version='0.4.0',
+      version='0.4.1',
       description='a Pythonic logger',
       author='Peter Fein',
       author_email='pfein@pobox.com',
       url='http://twiggy.wearpants.org',
-      download_url='http://python-twiggy.googlecode.com/files/Twiggy-0.4.0.tar.gz',
+      download_url='http://python-twiggy.googlecode.com/files/Twiggy-0.4.1.tar.gz',
       packages=['twiggy', 'twiggy.lib', 'twiggy.features'],
       license = "BSD",
       classifiers = [
