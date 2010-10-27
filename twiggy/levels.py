83b68be94f59aa1b57e910c66831c43a56c9599b
Peter Fein <pete@wearpants.org>
2010-10-26 20:28:27 -0700

    better test coverage for levels

--- twiggy/levels.py
+++ twiggy/levels.py
@@ -22,11 +22,20 @@ class LogLevel(object):
     def __repr__(self):
         return "<LogLevel %s>"%self.__name
 
-    def __cmp__(self, other):
+    def __lt__(self, other):
         if not isinstance(other, LogLevel):
             return NotImplemented
         else:
-            return cmp(self.__value, other.__value)
+            return self.__value < other.__value
+
+    def __eq__(self, other):
+        if not isinstance(other, LogLevel):
+            return NotImplemented
+        else:
+            return self.__value == other.__value
+
+    def __hash__(self):
+        return hash(self.__value) # pragma: no cover
 
 def name2level(name):
     """return a `LogLevel` from a case-insensitve string"""
