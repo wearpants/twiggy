e342700732c46f0cf76594240ad75603807b7518
Peter Fein <pete@wearpants.org>
2010-11-03 00:35:50 -0700

    make coverage script take a parameter

--- scripts/cover-twiggy-tests.sh
+++ scripts/cover-twiggy-tests.sh
@@ -2,7 +2,7 @@
 
 # find the unittest module
 coverage erase
-COVERAGE_PROCESS_START=.coveragerc coverage run scripts/unittest_main.py discover
+COVERAGE_PROCESS_START=.coveragerc coverage run scripts/unittest_main.py $@
 coverage combine
 coverage html
 
