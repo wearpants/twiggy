449ba59aced424ea7e601ea2a49f79b9bcd0aeef
Peter Fein <pete@wearpants.org>
2010-11-08 09:05:11 -0800

    use -b in script

--- scripts/run-twiggy-tests.sh
+++ scripts/run-twiggy-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TWIGGY_UNDER_TEST=1 python -m unittest discover
+TWIGGY_UNDER_TEST=1 python -m unittest discover -b
 sphinx-build -b doctest -d doc/_build/doctrees doc doc/_build/doctest
 
