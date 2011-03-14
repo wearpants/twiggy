5583c3fdab726e9042cc380d9147c4d213d807bd
Ian Foote <ianfoote@f2s.com>
2011-03-14 15:15:18 +0000

    Added tests for style aliases.

--- scripts/run-twiggy-tests.sh
+++ scripts/run-twiggy-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TWIGGY_UNDER_TEST=1 python -m unittest discover -b
+TWIGGY_UNDER_TEST=1 python2.7 -m unittest discover -b
 sphinx-build -b doctest -d doc/_build/doctrees doc doc/_build/doctest
 
