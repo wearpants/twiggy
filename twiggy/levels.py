73e37d4f904d2050cba6028601f5e2db50e53020
Toshio Kuratomi <a.badger@gmail.com>
2017-11-26 17:45:03 -0800

    Bring levels testing up to 100%

--- twiggy/levels.py
+++ twiggy/levels.py
@@ -3,8 +3,41 @@ Levels include (increasing severity): ``DEBUG``, ``INFO``, ``NOTICE``, ``WARNING
 ``CRITICAL``, ``DISABLED``
 """
 
+from six import PY3, with_metaclass
 
-class LogLevel(object):
+
+class LogLevelMeta(type):
+    """
+    Metaclass that aids in making comparisons work the same in Python2 and Python3
+
+    Python3 raises TypeError when unorderable types are compared via lt, gt, le, ge.
+    Python2 picks an order but it doesn't always make much sense.
+
+    In Python3, we only need the rich comparison operators to get this behaviour.
+
+    In Python2, we use the __cmp__ function to raise TypeError for lt, gt, le, and ge.
+    We define __eq__ and __ne__ on their own since those should just say that a LogLevel is never
+    equal to a non-LogLevel.
+    """
+    def __new__(meta, name, bases, dct):
+        cls = super(LogLevelMeta, meta).__new__(meta, name, bases, dct)
+
+        if PY3:  # pragma: no py2 cover
+            cls.__lt__ = cls._lt
+            cls.__gt__ = cls._gt
+            cls.__le__ = cls._le
+            cls.__ge__ = cls._ge
+            del cls.__cmp__
+        else:  # pragma: no py3 cover
+            del cls._lt
+            del cls._gt
+            del cls._le
+            del cls._ge
+
+        return cls
+
+
+class LogLevel(with_metaclass(LogLevelMeta, object)):
     """A log level. Users should *not* create new instances.
 
     Levels are opaque; they may be compared to each other, but nothing else.
@@ -24,25 +57,25 @@ class LogLevel(object):
     def __repr__(self):
         return "<LogLevel %s>" % self.__name
 
-    def __lt__(self, other):
+    def _lt(self, other):  # pragma: no py2 cover
         if not isinstance(other, LogLevel):
             return NotImplemented
         else:
             return self.__value < other.__value
 
-    def __le__(self, other):
+    def _le(self, other):  # pragma: no py2 cover
         if not isinstance(other, LogLevel):
             return NotImplemented
         else:
             return self.__value <= other.__value
 
-    def __gt__(self, other):
+    def _gt(self, other):  # pragma: no py2 cover
         if not isinstance(other, LogLevel):
             return NotImplemented
         else:
             return self.__value > other.__value
 
-    def __ge__(self, other):
+    def _ge(self, other):  # pragma: no py2 cover
         if not isinstance(other, LogLevel):
             return NotImplemented
         else:
@@ -50,15 +83,26 @@ class LogLevel(object):
 
     def __eq__(self, other):
         if not isinstance(other, LogLevel):
-            return NotImplemented
+            return False
         else:
             return self.__value == other.__value
 
     def __ne__(self, other):
         if not isinstance(other, LogLevel):
-            return NotImplemented
+            return True
         else:
             return self.__value != other.__value
+
+    def __cmp__(self, other):  # pragma: no py3 cover
+        # Python 2 only
+        if not isinstance(other, LogLevel):
+            raise TypeError('Unorderable types LogLevel() and %s' % type(other))
+        elif self.__value < other.__value:
+            return -1
+        elif self.__value > other.__value:
+            return 1
+        else:
+            return 0
 
     def __hash__(self):
         return hash(self.__value)
